0f10ca111eb7e7608ad6bf2fc6285fc9394a26f1
tomek.larysz <ttomek.larysz@gmail.com>
2023-07-10 18:18:57 +0200

    scrap urls and prices of rx 6700xt from morele.net

--- tracker.py
+++ tracker.py
@@ -1,1 +1,22 @@
-print('Hello')
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://www.morele.net/kategoria/karty-graficzne-12/,,,,,,,,0,,,,8143O1777802/1/')
+soup = BeautifulSoup(r.content, features="lxml")
+
+links = []
+titles = []
+prices = []
+
+for link in soup.find_all('a', class_='productLink'):
+    if "https://www.morele.net"+link.get('href') not in links:
+        links.append("https://www.morele.net"+link.get('href'))
+    
+    if link.get('title') not in titles:
+        titles.append(link.get('title'))
+
+
+for link in links:
+    page = requests.get(link)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    price = soup.find(class_="product-price").get_text().strip()
